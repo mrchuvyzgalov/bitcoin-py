2e84390e6bc51e894290f42e3268f939c98edcc1
mrchuvyzgalov <mr.chuvyzgalov@gmail.com>
2025-07-02 16:25:52 +0200

    Add code of bitcoin

---
+++ constants.py
@@ -0,0 +1,45 @@
+class MessageType:
+    TX = "tx"
+    BLOCK = "block"
+    REQUEST_CHAIN = "request_chain"
+    CHAIN = "chain"
+    MINING = "mining"
+    REBROADCAST = "rebroadcast"
+    FINALISE_BLOCK = "finalize_block"
+    DISCONNECT = "disconnect"
+
+class MessageField:
+    TYPE = "type"
+    DATA = "data"
+
+class TxInputField:
+    TX_ID = "tx_id"
+    INDEX = "index"
+    SIGNATURE = "signature"
+    PUBKEY = "pubkey"
+
+class TxOutputField:
+    AMOUNT = "amount"
+    ADDRESS = "address"
+
+class TxField:
+    INPUTS = "inputs"
+    OUTPUTS = "outputs"
+    METADATA = "metadata"
+
+class MetadataType:
+    HEIGHT = "height"
+
+class BlockField:
+    INDEX = "index"
+    PREVIOUS_HASH = "previous_hash"
+    TRANSACTIONS = "transactions"
+    NONCE = "nonce"
+    TIMESTAMP = "timestamp"
+
+class BlockchainField:
+    BLOCKS = "blocks"
+
+class DisconnectField:
+    HOST = "host"
+    PORT = "port"
